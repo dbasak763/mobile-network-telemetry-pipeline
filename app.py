f1d4a6ab898b1173ae986757fa0e4313ef01cfba
dbasak763 <67080212+dbasak763@users.noreply.github.com>
2023-06-26 16:39:07 -0500

    Update app.py(Added REST API to send JSONSchema from UE to appserver)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
 app = Flask(__name__)
@@ -12,9 +13,10 @@ gauge_metric = Gauge('my_data_table_metric', 'My data table metric', ['name'],
 @app.route('/')
 def hello():
     data_table = [
-        {'id': 1, 'name': 'Entry 1'},
-        {'id': 2, 'name': 'Entry 2'},
-        {'id': 3, 'name': 'Entry 3'},
+        {'id': 2, 'name': 'Entry 1'},
+        {'id': 3, 'name': 'Entry 2'},
+        {'id': 4, 'name': 'Entry 3'},
+        {'id': 15, 'name': 'Entry 4'}
     ]
 
     # Transform data table to Prometheus metrics
@@ -26,7 +28,20 @@ def hello():
     #or if you want to filter metrics for this job name
     push_to_gateway('10.116.11.204:9091', job='my-flask-app', registry=registry)
 
-    return 'Hello, World!'
+    return 'Hello, Worldy!'
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    #Allows access to JSON payload sent in the request
+    data = request.json
+    #Process the received data
+    
+    #Construct the response JSON including received data
+    response = {
+        'message': 'Data received successfully',
+        'data': data
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050)
