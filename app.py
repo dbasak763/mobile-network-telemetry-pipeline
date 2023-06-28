e326aaa8f5123325fe27ce47a8f684edb32a0a86
dbasak763 <67080212+dbasak763@users.noreply.github.com>
2023-06-28 14:00:41 -0500

    Update app.py(i can see gauge metric and info metric labels showing up in Prometheus UI)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+from prometheus_client import CollectorRegistry, Gauge, Info, push_to_gateway
 from jsonschema import validate, ValidationError
 from schema import schema
 
@@ -8,15 +8,17 @@ app = Flask(__name__)
 # Create Prometheus registry
 registry = CollectorRegistry()
 
-# Create the gauge metric for the data table
+# Create the gauge metric for numerical values
 gauge_metric = Gauge('my_data_table_metric', 'My data table metric',
                      ['ipaddress', 'param'], registry=registry)
 
+# Create the info metric for string values
+info_metric = Info('my_data_table_string_metric', 'My data table string metric',
+                   ['ipaddress', 'param'], registry=registry)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
-
 
 @app.route('/data', methods=['POST'])
 def receive_data():
@@ -34,33 +36,34 @@ def receive_data():
     lte_params = data['lte_params']
     ip_address = data['ip-address']
 
-    #debug statement
-    #return jsonify({'message': 'Reached this point'})
-    #Error happening when setting gauge metric values with labels
     # Set the gauge metric values with labels
+    gauge_metric.labels(ipaddress=ip_address, param='latitude').set(float(location['latitude']))
+    gauge_metric.labels(ipaddress=ip_address, param='longitude').set(float(location['longitude']))
+    gauge_metric.labels(ipaddress=ip_address, param='altitude').set(float(location['altitude']))
+    gauge_metric.labels(ipaddress=ip_address, param='accuracy').set(float(location['accuracy']))
+    gauge_metric.labels(ipaddress=ip_address, param='Fc').set(float(lte_params['Fc']))
+    gauge_metric.labels(ipaddress=ip_address, param='EarFcn').set(float(lte_params['EarFcn']))
+    gauge_metric.labels(ipaddress=ip_address, param='TimeAdv').set(float(lte_params['TimeAdv']))
+    gauge_metric.labels(ipaddress=ip_address, param='pci').set(float(lte_params['pci']))
+    gauge_metric.labels(ipaddress=ip_address, param='rsrp').set(float(lte_params['rsrp']))
+    gauge_metric.labels(ipaddress=ip_address, param='rsrq').set(float(lte_params['rsrq']))
+    gauge_metric.labels(ipaddress=ip_address, param='rssi').set(float(lte_params['rssi']))
+    gauge_metric.labels(ipaddress=ip_address, param='snr').set(float(lte_params['snr']))
+    gauge_metric.labels(ipaddress=ip_address, param='cqi').set(float(lte_params['cqi']))
+    # Set other numerical values using gauge_metric.labels().set()
 
-    #gauge_metric.labels(ipaddress=ip_address, param='model').set(device_info['model'])
-    
-    #gauge_metric.labels(ipaddress=ip_address, param='android').set(device_info['android'])
-    #gauge_metric.labels(ipaddress=ip_address, param='hardware').set(device_info['hardware'])
-    #gauge_metric.labels(ipaddress=ip_address, param='latitude').set(location['latitude'])
-    #gauge_metric.labels(ipaddress=ip_address, param='longitude').set(location['longitude'])
-    #gauge_metric.labels(ipaddress=ip_address, param='altitude').set(location['altitude'])
-    #gauge_metric.labels(ipaddress=ip_address, param='accuracy').set(location['accuracy'])
-    #gauge_metric.labels(ipaddress=ip_address, param='timestamp').set(data['device_information']['timestamp'])
-    #gauge_metric.labels(ipaddress=ip_address, param='mcc').set(lte_params['mcc'])
-    #gauge_metric.labels(ipaddress=ip_address, param='mnc').set(lte_params['mnc']) 
-    #gauge_metric.labels(ipaddress=ip_address, param='band').set(lte_params['band'])
-    #gauge_metric.labels(ipaddress=ip_address, param='Fc').set(lte_params['Fc'])
-    #gauge_metric.labels(ipaddress=ip_address, param='EarFcn').set(lte_params['EarFcn'])
-    #gauge_metric.labels(ipaddress=ip_address, param='TimeAdv').set(lte_params['TimeAdv'])
-    #gauge_metric.labels(ipaddress=ip_address, param='tac').set(lte_params['tac'])
-    #gauge_metric.labels(ipaddress=ip_address, param='pci').set(lte_params['pci'])
-    #gauge_metric.labels(ipaddress=ip_address, param='rsrp').set(lte_params['rsrp'])
-    #gauge_metric.labels(ipaddress=ip_address, param='rsrq').set(lte_params['rsrq'])
-    #gauge_metric.labels(ipaddress=ip_address, param='rssi').set(lte_params['rssi'])
-    #gauge_metric.labels(ipaddress=ip_address, param='snr').set(lte_params['snr'])
-    #gauge_metric.labels(ipaddress=ip_address, param='cqi').set(lte_params['cqi'])
+    # Set the info metric values with labels
+    info_metric.labels(ipaddress=ip_address, param='model').info({'value': device_info['model']})
+    info_metric.labels(ipaddress=ip_address, param='android').info({'value': device_info['android']})
+    info_metric.labels(ipaddress=ip_address, param='hardware').info({'value': device_info['hardware']})
+    info_metric.labels(ipaddress=ip_address, param='timestamp').info({'value': data['device_information']['timestamp']})
+    info_metric.labels(ipaddress=ip_address, param='mcc').info({'value': lte_params['mcc']})
+    info_metric.labels(ipaddress=ip_address, param='mnc').info({'value': lte_params['mnc']})
+    info_metric.labels(ipaddress=ip_address, param='band').info({'value': lte_params['band']})
+    info_metric.labels(ipaddress=ip_address, param='band').info({'value': lte_params['band']})
+    info_metric.labels(ipaddress=ip_address, param='tac').info({'value': lte_params['tac']})
+    info_metric.labels(ipaddress=ip_address, param='eci').info({'value': lte_params['eci']})
+    # Set other string values using info_metric.labels().info()
 
     # Push all metrics to the Pushgateway
     push_to_gateway('10.116.11.204:9091', job='my-flask-app', registry=registry)
@@ -71,7 +74,6 @@ def receive_data():
         'message': 'Data received successfully'
     }
     return jsonify(response)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050)
